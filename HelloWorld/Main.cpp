ef5ffaa7c294f1c6902b9a2ee001a74f681226c4
Kali <24emal@gmail.com>
2022-07-04 17:51:37 +0200

    create and implement func Fibonacci() to Math.h

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include "Log.h"
-
-int Multiply(int a, int b);
+#include "Math.h"
 
 int main()
 {
 	//std::cout << "Hello World" << std::endl;
 	Log("Hello World 2");
 	int res = Multiply(5, 8);
-	std::cout << res;
+	std::cout << res << std::endl;
+
+	float f = 5.0;
+
+	int s = sizeof(char);
+	std::cout << "sizeof float = " << s << std::endl;
+
+	MultiplyAndLog(3, 9);
+	MultiplyAndLog(12, 15);
+	MultiplyAndLog(8, 6);
+
+	Fibonacci();
+
 	std::cin.get();
 }
--- HelloWorld/Math.cpp
+++ HelloWorld/Math.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
 void Log(const char* message);
 
 int Multiply(int a, int b) {
 	Log("multiply");
 	return a * b;
+}
+
+void MultiplyAndLog(int a, int b) {
+	int result = a * b;
+	std::cout << result << std::endl;
+}
+
+void Fibonacci(void) {
+	std::cout << "Fibonacci" << std::endl;
+
+	/*
+	* f(0) = 0
+	* f(1) = 1
+	* f(n) = f(n-1) + f(n-2) for n >=2
+	*/
+
+	int i, n = 16;
+	int fibonacciNumbers[32] = { 0, 1 };
+
+	for (i = 2; i <= n; ++i)
+	{
+		fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];
+		std::cout << "fibonacciNumbers[" << i << "] = " << fibonacciNumbers[i] << std::endl;
+	}
+
 }
